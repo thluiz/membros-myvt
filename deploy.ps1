4337ace289334f7c7ed7546578ed21df7338558b
Thiago Silva <contato@thluiz.com>
2019-12-27 20:08:59 -0300

    smallfix in deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@ Set-Location Membros/fuse
 
 ng build --aot --prod 
 
-Set-Location ..
+Set-Location ../..
 
 git add -f wwwroot/. 
 
@@ -12,3 +12,4 @@ git commit -m "Deploy"
 git push --force dokku master 
 
 git reset --hard HEAD~1
+
