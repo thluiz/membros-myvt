ed2a99e03a9eda2c66e14a04b1ab7e57565dae24
Thiago Silva <contato@thluiz.com>
2019-12-27 20:02:06 -0300

    Adding deploy script

---
+++ deploy.ps1
@@ -0,0 +1,14 @@
+﻿
+Set-Location Membros/fuse
+
+ng build --aot --prod 
+
+Set-Location ..
+
+git add -f wwwroot/. 
+
+git commit -m "Deploy" 
+
+git push --force dokku master 
+
+git reset --hard HEAD~1
